2a10f1e90c9b376edd68e34c31ee410f057b2a58
RonaldTheodoro <ronaldtheodoro@gmail.com>
2016-01-10 18:02:57 -0200

    Adição da função client_sender

--- cap01/bhpnet.py
+++ cap01/bhpnet.py
@@ -30,4 +30,79 @@ def usage():
     print 'echo "ABCDEFGHI" | ./bhpnet.py -t 192.168.0.1 -p 135'
     sys.exit(0)
 
-usage()
+
+def client_sender(buffer):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect((target, port))
+
+        if len(buffer):
+            client.send(buffer)
+
+        while True:
+            recv_len = 1
+            response = ''
+
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                response += data
+
+                if recv_len < 4096:
+                    break
+
+            print response,
+
+            buffer = raw_input('')
+            buffer += '\n'
+
+            client.send(buffer)
+
+    except:
+        print '[*] Exception! Exiting.'
+        client.close()
+
+
+def main():
+    global listen
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hle:t:p:cu', ['help', 'listen', 'execute', 'target', 'port', 'command', 'upload'])
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage()
+        elif o in ('-l', '--listen'):
+            listen = True
+        elif o in ('-e', '--execute'):
+            execute = a
+        elif o in ('-c', '--commandshell'):
+            command = True
+        elif o in ('-u', '--upload'):
+            upload_destination = a
+        elif o in ('-t', '--target'):
+            target = a
+        elif o in ('-p', '--port'):
+            port = int(a)
+        else:
+            assert False, 'Unhandled Option'
+
+    if not listen and len(target and port > 0):
+        buffer = sys.stdin.read()
+
+        client_sender(buffer)
+    if listen:
+        server_loop()
+main()
