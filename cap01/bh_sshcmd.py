b54e9001f14df19cf991d2734afbeb2806c5ad5b
RonaldTheodoro <ronaldtheodoro@gmail.com>
2016-01-10 22:15:56 -0200

    Acesso ssh simples com o paramiko

---
+++ cap01/bh_sshcmd.py
@@ -0,0 +1,24 @@
+import threading
+import paramiko
+import subprocess
+
+
+def ssh_command(ip, user, passwd, command):
+    client = paramiko.SSHClient()
+
+    # client.load_host_keys('/home/ronald/.ssh/known_hosts')
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        print ssh_session.recv(1024)
+
+    return
+
+ip = raw_input('Digite o Ip: ')
+user = raw_input('Digite o usuario: ')
+passwd = raw_input('Digite a senha do usuario %s: ' % user)
+command = raw_input('Digite o comando: ')
+
+ssh_command(ip, user, passwd, command)
